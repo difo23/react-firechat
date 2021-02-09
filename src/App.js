ddab5390c9d5f9785f3f7544d631be0bf918609d
difo23 <diffozanzan@gmail.com>
2021-02-09 12:24:19 -0400

    Add signIn and signOut

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import {
-  db,
-  googleAuthProvider,
-  firebase
-} from './config/firebase';
-import Button from './components/Button';
 
-//TODO: Hacer el login con Google.
+import Button from './components/Button';
+import { signInWithGoogle, signOut } from './login';
 
 function App() {
-
-  const signInWithGoogle = async () => {
-
-    // Coloca el lenguje de preferencia del dispositivo
-    firebase.auth().useDeviceLanguage();
-
-    // Incio el procedo de login dentro de un try ... catch
-
-    try {
-      await firebase.auth().signInWithPopup(googleAuthProvider);
-
-    } catch (e) {
-      console.error(e.message);
-    }
-
-  };
-
-
   // Renderizar el Componente Button
   return (
     <div>
-      <Button onClick={signInWithGoogle} > Sing in with Google</Button>
+
+      {
+        user ? (
+          <>
+            <Button onClick={signOut} > Sing in with Google</Button>
+            <p> Bienvenidos al CHAT!</p>
+          </>
+        ) : <Button onClick={signInWithGoogle} > Sing in with Google</Button>
+
+
+      }
+
     </div>
   );
 }

852778180b7400d9658153e5671d5bdfddc4f975
difo23 <diffozanzan@gmail.com>
2021-02-08 23:56:13 -0400

    Setup sign in with Google

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import {
   googleAuthProvider,
   firebase
 } from './config/firebase';
+import Button from './components/Button';
 
 //TODO: Hacer el login con Google.
 
 function App() {
+
+  const signInWithGoogle = async () => {
+
+    // Coloca el lenguje de preferencia del dispositivo
+    firebase.auth().useDeviceLanguage();
+
+    // Incio el procedo de login dentro de un try ... catch
+
+    try {
+      await firebase.auth.singnInWithGooglePopup(googleAuthProvider);
+
+    } catch (e) {
+      console.error(e.message);
+    }
+
+  };
+
+
+  // Renderizar el Componente Button
   return (
-    <div></div>
+    <div>
+      <Button onClick={signInWithGoogle} > Sing in with Google</Button>
+    </div>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({
+    onClick = null,
+    children = null
+}) =>
+(
+    <button onClick={onClick} >{children} </button>
+);
+
+export default Button;
